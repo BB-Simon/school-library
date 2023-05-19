76ff0e7e080ae454a9e23ac3fd5919309f66c3d7
BB-Simon <simon.chowdery@gmail.com>
2023-05-19 19:47:22 +0300

    Add classroom, rentals and book classes

--- student.rb
+++ student.rb
@@ -1,12 +1,21 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name: 'Unknow', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(_classroom, age, name: 'Unknow', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    unless classroom.students.include?(self)
+      classroom.students << self
+    end
   end
 end
