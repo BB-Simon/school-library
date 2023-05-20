10f4b2fb256e8d83bb6f3ae8c567fa6cbe5dced6
BB-Simon <simon.chowdery@gmail.com>
2023-05-20 05:30:34 +0300

    Add relationship between book, rental and person

--- rental.rb
+++ rental.rb
@@ -1,7 +1,17 @@
 class Rental
   attr_accessor :date
+  attr_reader :book, :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    unless book.rentals.include?(self)
+      book.rentals.push(self)
+    end
+
+    @person = person
+    unless person.rentals.include?(self)
+      person.rentals.push(self)
+    end
   end
 end
